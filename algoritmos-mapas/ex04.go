1cb5d070491142d389653475118ae16eb7c953c3
Tales Masoero <141031024+Tales25@users.noreply.github.com>
2023-10-30 22:57:42 -0300

    Update ex04.go

--- algoritmos-mapas/ex04.go
+++ algoritmos-mapas/ex04.go
@@ -3,11 +3,56 @@
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	sliceStr := []string{"amor", "roma", "Hello,", "mora", "abc", "World!", "cab", "bca"}
+
+	mapAnagram := findAnagrams(sliceStr)
+	fmt.Print(mapAnagram)
 }
 
-func findAnagrams(sliceStr []string) map[string]string {
-	mapAnagrms := make(map[string]string)
-	
+func findAnagrams(sliceStr []string) map[string][]string {
+	anagramMap := make(map[string][]string)
+
+	for i, word1 := range sliceStr {
+		for j, word2 := range sliceStr {
+			//se forem palavras diferentes (indices diferentes) e areAnagramas == true
+			if i != j && areAnagrams(word1, word2) {
+				anagramMap[word1] = append(anagramMap[word1], word2)
+			}
+		}
+	}
+
+	return anagramMap
+}
+
+func areAnagrams(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	//mapas
+	charCount1Map := countChars(str1)
+	charCount2Map := countChars(str2)
+
+	//conferindo se são anagramas
+	for chave, _ := range charCount1Map {
+		if charCount1Map[chave] != charCount2Map[chave] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// com essa função só preciso de um mapa e passa-los para strings diferentes
+func countChars(str string) map[string]int {
+	charCount := make(map[string]int)
+
+	for _, char := range str {
+		charCount[string(char)]++
+	}
+
+	return charCount
 }
